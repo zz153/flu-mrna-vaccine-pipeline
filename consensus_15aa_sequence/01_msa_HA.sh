6abb844566034b20451a43ee5ba22f3f35579648
Zohaib Rana <121222194+zz153@users.noreply.github.com>
2025-10-20 14:10:15 +1300

    Delete consensus_15aa_sequence directory

--- consensus_15aa_sequence/01_msa_HA.sh
+++
@@ -1,8 +0,0 @@
-#concatenate the sequences
-cat ../data/raw/HA_H1.fa ../data/raw/HA_H3.fa ../data/raw/HA_H5.fa ../data/raw/HA_H7.fa > ../data/processed/combined_HA.fa
-
-#count the number of sequences
-grep "^>" ../data/processed/combined_HA.fa | wc -l
-
-#run msa using mafft tool
-mafft --auto --thread -1 ../data/processed/combined_HA.fa > ../data/processed/combined_HA_aligned.fa
